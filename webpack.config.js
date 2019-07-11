faf0eb1799de50538f4e5aea0e1ae7b12b2cf084
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-07-11 14:58:49 +0530

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,88 @@
 //final bundle file output destination
 // console.log(__dirname);
 const path = require("path");
+const ExtractWebpackPlugin = require("extract-text-webpack-plugin");
 // console.log(path.resolve(__dirname, "dist"));
 // console.log(path.join("/foo", "bar", "/baz/asdf", "quux", "..", ".."));
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractWebpackPlugin("styles.css");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    //babel-loader
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
+  console.log(env);
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      //babel-loader
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
 
-      //loaders for css  and use provides array of loaders
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-    port: 8000
-  }
+        //loaders for css  and use provides array of loaders
+        {
+          test: /\.s?css$/,
+          //use: ["style-loader", "css-loader", "sass-loader"]
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map", //"cheap-module-eval-source-map"
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+      port: 8000
+    }
+  };
 };
+
+// module.exports = {
+//   entry: "./src/app.js",
+//   output: {
+//     path: path.join(__dirname, "public"),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     //babel-loader
+//     rules: [
+//       {
+//         loader: "babel-loader",
+//         test: /\.js$/,
+//         exclude: /node_modules/
+//       },
+
+//       //loaders for css  and use provides array of loaders
+//       {
+//         test: /\.s?css$/,
+//         use: ["style-loader", "css-loader", "sass-loader"]
+//       }
+//     ]
+//   },
+//   devtool: "cheap-module-eval-source-map",
+//   devServer: {
+//     contentBase: path.join(__dirname, "public"),
+//     historyApiFallback: true,
+//     port: 8000
+//   }
+// };
